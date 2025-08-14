b81820e3f27ab23a55764bfa17ebf42ebb81b8d9
Saksham Khare <sakshamkhare19@gmail.com>
2025-08-15 00:12:27 +0530

    check tests

--- vitest-express-tests/src/index.ts
+++ vitest-express-tests/src/index.ts
@@ -52,8 +52,8 @@ app.post("/sum", async (req: Request, res: Response): Promise<void> => {
 
   const response = await prismaClient.sum.create({
     data: {
-      a: parsedResponse.data.a, // testing tests through ci/ci
-      b: parsedResponse.data.b, // testing tests through ci/ci
+      a: parsedResponse.data.a, // testing tests through ci/ci, once more
+      b: parsedResponse.data.b, // testing tests through ci/ci, once more
       result: answer,
     },
   });
